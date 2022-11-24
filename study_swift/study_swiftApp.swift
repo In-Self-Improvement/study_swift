c74b83a0dab6fd6c762de7d20f1056698e773ffe
borobong2 <borobong230@gmail.com>
2022-11-24 19:45:48 +0900

    Initial Commit

---
+++ study_swift/study_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_swiftApp.swift
+//  study_swift
+//
+//  Created by bong on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct study_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
