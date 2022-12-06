6efd772f919cca1fb2b523773e62f1fdef173ec4
borobong2 <borobong230@gmail.com>
2022-12-06 23:36:56 +0900

    learn VStack, HStack

--- study_swift/ContentView.swift
+++ study_swift/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").fontWeight(.bold).font(.system(size:100))
-
+        HStack{
+            Text("1!").fontWeight(.bold).font(.system(size:50))
+            Text("2!").fontWeight(.bold).font(.system(size:60))
+            Text("3!").fontWeight(.bold).font(.system(size:70))
+        }
     }
 }
 
